a4f040754b8cc8baf5809e7e25060292c8495eb1
Felipe Nery Barcelos Araujo <felipenery@ufmg.br>
2023-08-11 18:44:20 -0300

    Primeiro testes do programa! Download DVR

--- main.py
+++ main.py
@@ -1,22 +1,90 @@
-from pynput import keyboard
+from pynput.keyboard import Key, Listener 
+import datetime
+import threading
+import time
+import ffmpeg
+import os
 
-def on_press(key):
+lock_capture = threading.Lock()
+
+def upload_drive_video(source):
+    print("Upload!")
+
+def capture_video(cameras, ido):
+    print("Quero Conquistar", ido)
+    agora = datetime.datetime.now()
+    #Retirar os 10 minutos atrás na prática
+    futuro = agora + datetime.timedelta(seconds = 2) - datetime.timedelta(minutes=10)
+    passado = agora - datetime.timedelta(seconds = 19) - datetime.timedelta(minutes=10)
+    #adicionar um sleep de 5 minutos
+    #time.sleep(300) #300s = 5 minutos
+    lock_capture.acquire()
+    print("Conquistei!", ido)
+    start_time = str(passado.year) + str(passado.month).zfill(2) + str(passado.day).zfill(2) + "T" + str(passado.hour).zfill(2) + str(passado.minute).zfill(2) + str(passado.second).zfill(2)
+    end_time = str(futuro.year) + str(futuro.month).zfill(2) + str(futuro.day).zfill(2) + "T" + str(futuro.hour).zfill(2) + str(futuro.minute).zfill(2) + str(futuro.second).zfill(2)
+    server_ip = "192.168.2.108:554"
+    username = "admin"
+    password = "fred8808"
+    url = "rtsp:/" + username + ":" + password + "@" + server_ip + "/ISAPI/streaming/tracks/" + str(cameras) + "?starttime=" + start_time + "Z&endtime=" + end_time + "Z"
+    source = "tempfiles/" + "2" + "/" + str(cameras) + "/"
+    if not os.path.exists(source):
+       os.makedirs(source) 
     try:
-        print('Alphanumeric key pressed: {0} '.format(
-            key.char))
-    except AttributeError:
-        print('special key pressed: {0}'.format(
-            key))
+       stream = ffmpeg.input(
+           url,
+           rtsp_transport='tcp',
+           listen_timeout=-1,
+           t=15 
+       )
+       stream = ffmpeg.output(stream, 
+                              source + start_time + '.mp4',
+                              map='0:v',
+                              acodec='copy',
+                              vcodec='copy')
+       ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+    except ffmpeg.Error as e:
+       print('stdout:', e.stdout.decode('utf8'))
+       print('stderr:', e.stderr.decode('utf8'))
+    print('Video salvo!', ido)
+    lock_capture.release()
+    print('Mutex liberado!', ido)
+     
+def thread_capture_soccer(name):
+
+    print("Thread Capture Soccer", name, " start")
+    time.sleep(2)
+    print("Thread ", name, " wakeup")
+
+global ilo
+def on_press(key):
+        global ilo
+        print('\nYou Entered {0}'.format( key))
+        if key == Key.f7: 
+            print("\n\nF7\n\n")
+            x = threading.Thread(target=capture_video, args=(701,ilo))
+            ilo+=1
+            x.start()
+        if key == Key.f8: 
+            print("\n\nF7\n\n")
+            #x = threading.Thread(target=thread_test, args=(2,))
+            #x.start()
 
 def on_release(key):
-    print('Key released: {0}'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
+     if key == Key.esc:
+         # Stop listener
+         return False
 
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+ilo = 1
+with Listener(on_press = on_press, on_release=on_release) as listener: 
+    listener.join() 
+# agora = datetime.datetime.now()
+# futuro = agora + datetime.timedelta(seconds = 2)
+# passado = agora - datetime.timedelta(seconds = 19)
+# print("Passado: ", passado)
+# print("Agora: ", agora)
+# print("Futuro: ", futuro)
+# start_time = str(passado.year) + str(passado.month).zfill(2) + str(passado.day).zfill(2) + "T" + str(passado.hour).zfill(2) + str(passado.minute).zfill(2) + str(passado.second).zfill(2)
+# end_time = str(futuro.year) + str(futuro.month).zfill(2) + str(futuro.day).zfill(2) + "T" + str(futuro.hour).zfill(2) + str(futuro.minute).zfill(2) + str(futuro.second).zfill(2)
+# print(start_time)
+# print(end_time)
